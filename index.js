3ca4bbd0a6f941bbde2a676aa89c6a52efcbcb5f
royke <keane032@gmail.com>
2024-01-23 20:08:52 -0300

    nov layout

--- index.js
+++ index.js
@@ -36,3 +36,20 @@ typingPromises(message, 140).forEach(promise => {
     }
   });
 });
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      const targetId = this.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+
+      if (targetElement) {
+          window.scrollTo({
+              top: targetElement.offsetTop - 100, // Ajuste conforme necessário
+              behavior: 'smooth' // Rola suavemente
+          });
+      }
+  });
+});
