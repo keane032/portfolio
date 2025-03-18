52e4a2da0d15e915a0916503bcf3123e428c6cc5
royke <keane032@gmail.com>
2025-03-18 13:34:08 -0300

    mudanca de layout

--- index.js
+++ index.js
@@ -1,40 +1,42 @@
 
-  document.getElementById("repositorioJogo").onclick = function () {
-      window.open('https://github.com/keane032/dungeon_explorer');
-  };
+  // document.getElementById("repositorioJogo").onclick = function () {
+  //     window.open('https://github.com/keane032/dungeon_explorer');
+  // };
 
   document.getElementById("app").onclick = function () {
     window.open('https://play.google.com/store/apps/details?id=com.br.rpgboard');
 };
 
+// document.getElementById("app").addEventListener('click', () => {
+//   window.location = 'http://pt.stackoverflow.com'
+// })
+
 document.getElementById("angular").onclick = function () {
   window.open('https://keane032.github.io/angular-portfolio/?name=keane032');
 };
 
+//   $(document).ready(function(){
+//     $('#projects').slick({
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//         autoplay: true,
+//         autoplaySpeed: 2000,
+//         dots: true,
+//         arrows: false
+//     });
+// });
 
-  $(document).ready(function(){
-    $('#projects').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        dots: true,
-        arrows: false
-    });
-});
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//   anchor.addEventListener('click', function (e) {
+//       e.preventDefault();
+//       const targetId = this.getAttribute('href').substring(1);
+//       const targetElement = document.getElementById(targetId);
 
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-
-      const targetId = this.getAttribute('href').substring(1);
-      const targetElement = document.getElementById(targetId);
-
-      if (targetElement) {
-          window.scrollTo({
-              top: targetElement.offsetTop - 100, // Ajuste conforme necessário
-              behavior: 'smooth' // Rola suavemente
-          });
-      }
-  });
-});
+//       if (targetElement) {
+//           window.scrollTo({
+//               top: targetElement.offsetTop - 100, // Ajuste conforme necessário
+//               behavior: 'smooth' // Rola suavemente
+//           });
+//       }
+//   });
+// });
