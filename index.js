b45a714d0948d823bd22dbaaac5312e462b2b0ed
royke <keane032@gmail.com>
2025-05-19 21:40:57 -0300

    responsividade

--- index.js
+++ index.js
@@ -16,6 +16,16 @@ document.getElementById("integrasus").onclick = function () {
   window.open('https://integrasus.saude.ce.gov.br/#/home');
 };
 
+function showMenu(){
+  let menu = document.querySelector('.mobile-menu');
+  if (menu.classList.contains('open')) {
+        menu.classList.remove('open');
+        // document.querySelector('.icon').src = "assets/img/menu_white_36dp.svg";
+    } else {
+        menu.classList.add('open');
+        // document.querySelector('.icon').src = "assets/img/close_white_36dp.svg";
+    }
+}
 //   $(document).ready(function(){
 //     $('#projects').slick({
 //         slidesToShow: 1,
